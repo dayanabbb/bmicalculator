64266ad5fac0a5b7e4dc27cdf97885c35b792b5b
Dayana Belyalova <124685989+dayanabbb@users.noreply.github.com>
2023-12-30 16:50:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-// Serve static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+
 const bmiRouter = require('./routes/bmi');
 app.use('/', bmiRouter);
 
-// Start server
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
